1795bc077f1e92c7ed14d248efa93e2971849cd4
Adriana <vadriana258@gmail.com>
2025-05-04 23:41:04 +0200

    fallbackredirecturl added

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ createRoot(document.getElementById("root")).render(
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       afterSignOutUrl="/design-courses-site"
-      fallbackRedirectUrl="/design-courses-site"
+      fallbackRedirectUrl="https://adrianav1999.github.io/design-courses-site/"
     >
       <AppContextProvider>
         <App />
