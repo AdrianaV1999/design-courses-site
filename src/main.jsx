bf6d99767360d6f0bbf44a0141108288db0a06a9
Adriana <vadriana258@gmail.com>
2025-05-05 21:05:52 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -16,11 +16,6 @@ createRoot(document.getElementById("root")).render(
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       afterSignOutUrl="/design-courses-site"
-      forceRedirectUrl="https://adrianav1999.github.io/design-courses-site/"
-      fallbackRedirectUrl="https://adrianav1999.github.io/design-courses-site/"
-      allowedRedirectOrigins={[
-        "https://adrianav1999.github.io/design-courses-site/",
-      ]}
     >
       <AppContextProvider>
         <App />
