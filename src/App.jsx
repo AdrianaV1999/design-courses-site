cecf40bb2ee43924ab1af662775c4a698dce158e
Adriana <vadriana258@gmail.com>
2025-05-05 18:23:22 +0200

    handle routes for authenticated and guest users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route, Routes, useMatch, Navigate } from "react-router-dom";
-import { SignedIn, useUser } from "@clerk/clerk-react"; // Clerk imports for authentication
+import { Route, Routes, useMatch } from "react-router-dom";
 import Home from "./pages/student/Home";
 import CoursesList from "./pages/student/CoursesList";
 import CourseDetails from "./pages/student/CourseDetails";
@@ -13,54 +12,40 @@ import AddCourse from "./pages/educator/AddCourse";
 import MyCourses from "./pages/educator/MyCourses";
 import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
 import Navbar from "./components/student/Navbar";
-
+import { SignedIn } from "@clerk/clerk-react";
 const App = () => {
-  const { isSignedIn } = useUser(); // Proveravamo da li je korisnik prijavljen
   const isEducatorRoute = useMatch("/educator/*");
-
   return (
     <div className="text-default min-h-screen bg-white">
       {!isEducatorRoute && <Navbar />}
 
       <Routes>
-        {/* Public Routes (accessible by all users) */}
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />
         <Route path="/course-list/:input" element={<CoursesList />} />
         <Route path="/course/:id" element={<CourseDetails />} />
-        <Route path="/loading/:path" element={<Loading />} />
-
-        {/* Protected Routes (accessible only by authenticated users) */}
         <Route
           path="/my-enrollments"
-          element={isSignedIn ? <MyEnrollments /> : <Navigate to="/" />}
+          element={
+            <SignedIn>
+              <MyEnrollments />
+            </SignedIn>
+          }
         />
         <Route
           path="/player/:courseId"
-          element={isSignedIn ? <Player /> : <Navigate to="/" />}
+          element={
+            <SignedIn>
+              <Player />
+            </SignedIn>
+          }
         />
-
-        {/* Educator Routes */}
-        <Route
-          path="/educator"
-          element={isSignedIn ? <Educator /> : <Navigate to="/" />}
-        >
-          <Route
-            path="/educator"
-            element={isSignedIn ? <Dashboard /> : <Navigate to="/" />}
-          />
-          <Route
-            path="add-course"
-            element={isSignedIn ? <AddCourse /> : <Navigate to="/" />}
-          />
-          <Route
-            path="my-courses"
-            element={isSignedIn ? <MyCourses /> : <Navigate to="/" />}
-          />
-          <Route
-            path="student-enrolled"
-            element={isSignedIn ? <StudentsEnrolled /> : <Navigate to="/" />}
-          />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
         </Route>
       </Routes>
     </div>
--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -3,17 +3,25 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../../components/educator/Navbar";
 import Sidebar from "../../components/educator/Sidebar";
 import Footer from "../../components/educator/Footer";
+import { SignedIn } from "@clerk/clerk-react";
+
 const Educator = () => {
   return (
     <div className="text-default min-h-screen bg-white">
       <Navbar />
       <div className="flex">
-        <Sidebar />
+        <SignedIn>
+          <Sidebar />
+        </SignedIn>
         <div className="flex-1">
-          <Outlet />
+          <SignedIn>
+            <Outlet />
+          </SignedIn>
         </div>
       </div>
-      <Footer />
+      <SignedIn>
+        <Footer />
+      </SignedIn>
     </div>
   );
 };
