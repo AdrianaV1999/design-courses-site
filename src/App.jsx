54e15843c94ce16287f8c4c40df87607fe1f4266
Adriana <vadriana258@gmail.com>
2025-05-05 17:14:50 +0200

    set up routes for authenticated access

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes, useMatch } from "react-router-dom";
+import { Route, Routes, useMatch, Navigate } from "react-router-dom";
+import { SignedIn, useUser } from "@clerk/clerk-react"; // Clerk imports for authentication
 import Home from "./pages/student/Home";
 import CoursesList from "./pages/student/CoursesList";
 import CourseDetails from "./pages/student/CourseDetails";
@@ -14,24 +15,52 @@ import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
 import Navbar from "./components/student/Navbar";
 
 const App = () => {
+  const { isSignedIn } = useUser(); // Proveravamo da li je korisnik prijavljen
   const isEducatorRoute = useMatch("/educator/*");
+
   return (
     <div className="text-default min-h-screen bg-white">
       {!isEducatorRoute && <Navbar />}
 
       <Routes>
+        {/* Public Routes (accessible by all users) */}
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />
         <Route path="/course-list/:input" element={<CoursesList />} />
         <Route path="/course/:id" element={<CourseDetails />} />
-        <Route path="/my-enrollments" element={<MyEnrollments />} />
-        <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
-        <Route path="/educator" element={<Educator />}>
-          <Route path="/educator" element={<Dashboard />} />
-          <Route path="add-course" element={<AddCourse />} />
-          <Route path="my-courses" element={<MyCourses />} />
-          <Route path="student-enrolled" element={<StudentsEnrolled />} />
+
+        {/* Protected Routes (accessible only by authenticated users) */}
+        <Route
+          path="/my-enrollments"
+          element={isSignedIn ? <MyEnrollments /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/player/:courseId"
+          element={isSignedIn ? <Player /> : <Navigate to="/" />}
+        />
+
+        {/* Educator Routes */}
+        <Route
+          path="/educator"
+          element={isSignedIn ? <Educator /> : <Navigate to="/" />}
+        >
+          <Route
+            path="/educator"
+            element={isSignedIn ? <Dashboard /> : <Navigate to="/" />}
+          />
+          <Route
+            path="add-course"
+            element={isSignedIn ? <AddCourse /> : <Navigate to="/" />}
+          />
+          <Route
+            path="my-courses"
+            element={isSignedIn ? <MyCourses /> : <Navigate to="/" />}
+          />
+          <Route
+            path="student-enrolled"
+            element={isSignedIn ? <StudentsEnrolled /> : <Navigate to="/" />}
+          />
         </Route>
       </Routes>
     </div>
